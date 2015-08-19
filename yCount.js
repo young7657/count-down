44c8fe3ec0be9dcedadcb556915544045b2e6789
youngliu <youngliu129@126.com>
2015-08-19 10:01:06 +0800

    增加转换函数

--- yCount.js
+++ yCount.js
@@ -1,3 +1,21 @@
 ;(function($, window) {
+	// 把时间间隔（毫秒数）转换成倒计时格式
+	function milliToCountDown(dur) {
+		var scale = [1000, 60, 60, 24, 30, 12];
+		var l = scale.length;
+		var result = {
+				milli: '00',sec: '00',mini: '00',hour: '00',
+				day: '00',month: '00',year: '00'
+			};
+		if (dur <= 0) {
+			return result;
+		}
+		for (var i = 0; i < l; i++) {
 
+		}
+	}
+
+	$.yCount = function() {
+
+	}
 })(jQuery, this);
